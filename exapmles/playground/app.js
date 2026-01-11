216f84b88c8d700d260c0a69bfc6609e8bc41c55
LarabiMergaoui <mergaouiarbi@gmail.com>
2026-01-11 16:44:16 +0100

    fixing for

--- exapmles/playground/app.js
+++ exapmles/playground/app.js
@@ -5,7 +5,7 @@ const [arr, setArr] = createSignal([1, 2, 3, 4, 5])
 
 createEffect(() => {
   if (className() === "blue") {
-    setArr([...arr(), 0])
+    // setArr([...arr(), 0])
   }
 })
 createEffect(() => {
@@ -23,13 +23,22 @@ const Map = dom({
     style: () => `width: 100px; height: 100px; background-color: ${className()};`,
     onClick: (e) => {
       setClassName(className() === "red" ? "blue" : "red");
+      setArr([...arr(), arr().length + 1]);
     }
   },
   children: For(arr, (item) => ({
     tag: "div",
-    children: () => item.value
+    attributes: {},
+    children: [item.toString()]
   }))
 })
+
+// console.log(For(arr, (item) => ({
+//     tag: "div",
+//     attributes: {},
+//     children: () => item.value
+//   }))()
+// )
 
 
 
